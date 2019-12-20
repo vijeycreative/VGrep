6264dab57344863e7fb962f29765319082eb1636
Vijendran  V <v.vijendran@anu.edu.au>
2019-12-20 19:49:34 +1100

    Fixed Error Handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main()
 {
     let args:Vec<String>=env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem in Parsing {}: ", err);
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In File {}", config.filename);
 
@@ -21,9 +25,14 @@ struct Config
     filename: String
 }
 
-fn parse_config(args:&[String]) -> Config
-{
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    return Config{query, filename};
+impl Config{
+    fn new(args:&[String]) -> Result<Config, &'static str>
+    {
+        if args.len() < 3{
+            return Err("Not Enough Arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        return Ok(Config{query, filename});
+    }
 }

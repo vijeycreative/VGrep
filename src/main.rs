70b0639cdde678199bdc8760561600c91ea637c6
Vijendran  V <v.vijendran@anu.edu.au>
2019-12-20 19:36:08 +1100

    Made VGrep Modular

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,25 @@ use std::io::prelude::*;
 fn main()
 {
     let args:Vec<String>=env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("In File {}", filename);
+    let config = parse_config(&args);
+    println!("Searching for {}", config.query);
+    println!("In File {}", config.filename);
 
-    let mut file = File::open(filename).expect("File Not Found");
+    let mut file = File::open(config.filename).expect("File Not Found");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Failed");
     println!{"{}", contents};
+}
+
+struct Config
+{
+    query: String,
+    filename: String
+}
+
+fn parse_config(args:&[String]) -> Config
+{
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    return Config{query, filename};
 }

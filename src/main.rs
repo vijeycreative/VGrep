b451afb91cd1908ab14f94b5150c10cc7ab50048
Vijendran  V <v.vijendran@anu.edu.au>
2019-12-20 19:23:04 +1100

    File IO Functionality Added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main()
+{
+    let args:Vec<String>=env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for {}", query);
+    println!("In File {}", filename);
+
+    let mut file = File::open(filename).expect("File Not Found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Failed");
+    println!{"{}", contents};
 }
